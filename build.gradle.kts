74fa4ae01fd0728eadd60855f260e06612771eef
John Woodell <mr.john.woodell@gmail.com>
2026-08-09 01:20:49 -0700

    Bump version to 0.5.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "com.netpress"
-version = "0.5.1"
+version = "0.5.2"
 
 kotlin {
     jvmToolchain(17)
@@ -104,7 +104,7 @@ compose.desktop {
             // not just local dev testing -- see docs/COWORK.md.
             targetFormats(TargetFormat.Msi, TargetFormat.Dmg, TargetFormat.Pkg)
             packageName = "Huck"
-            packageVersion = "0.5.1"
+            packageVersion = "0.5.2"
             // Hand-maintained, not computed -- see docs/COMMENTS.md.
             copyright = "© 2026 John Woodell"
 
